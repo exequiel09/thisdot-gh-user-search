e4dcb0d32134dd05809c2a4209a27e10090b1e47
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-07-01 15:18:05 +0800

    Implement initial working search functionality

--- src/app/modules/default/pages/index/index.component.ts
+++ src/app/modules/default/pages/index/index.component.ts
@@ -1,4 +1,39 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ClrDatagridStateInterface } from '@clr/angular';
+import isEqual from 'lodash-es/isEqual';
+import { Observable, of, Subject } from 'rxjs';
+import { auditTime, catchError, distinctUntilChanged, filter, pluck, switchMap } from 'rxjs/operators';
+
+export interface SearchResultItem {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
+}
+
+export interface SearchResults {
+  total_count: number;
+  incomplete_results: boolean;
+  items: SearchResultItem[];
+}
 
 @Component({
   templateUrl: './index.component.html',
@@ -6,12 +41,58 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IndexComponent implements OnInit {
-  users: Record<string, string>[] = [];
+  user$: Observable<SearchResults>;
+  refreshDataGrid$: Observable<ClrDatagridStateInterface>;
 
-  constructor() { }
+  private readonly _refreshDataGrid$ = new Subject<ClrDatagridStateInterface>();
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _httpClient: HttpClient,
+    private readonly _router: Router
+  ) {
+    this.refreshDataGrid$ = this._refreshDataGrid$.asObservable();
+
+    this.refreshDataGrid$
+      .pipe(
+        auditTime(500),
+
+        distinctUntilChanged<ClrDatagridStateInterface>(isEqual),
+
+        pluck('filters')
+      )
+      .subscribe(filters => {
+        if (typeof filters !== 'undefined' && filters.length > 0) {
+          this._router.navigate(['/'], {
+            queryParams: {
+              username: filters[0].value,
+            },
+          });
+        }
+      })
+      ;
+
+    this.user$ = this._activatedRoute.queryParams.pipe(
+      filter(queryParams => Object.keys(queryParams).length > 0 && typeof queryParams.username !== 'undefined'),
+
+      switchMap(queryParams => {
+        return this._httpClient.get<SearchResults>(`https://api.github.com/search/users?q=${queryParams.username}`).pipe(
+          catchError(() => of<SearchResults>({
+            total_count: 0,
+            incomplete_results: false,
+            items: [],
+          }))
+        );
+      })
+    );
+  }
 
   ngOnInit(): void { }
 
-  refreshDataGrid(): void { }
+  refreshDataGrid(state: ClrDatagridStateInterface): void {
+    this._refreshDataGrid$.next(state);
+  }
 
 }
+
+
