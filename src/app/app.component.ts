8b5c2192d39e1ccfbce61c45fb44fb43c17208e6
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-07-01 10:19:16 +0800

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tdgh-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'thisdot-gh-user-search';
+}
