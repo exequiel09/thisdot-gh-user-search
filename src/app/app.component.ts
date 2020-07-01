bb9f7a09020ac6977ee1fbbb64bd45a73dc8496e
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-07-01 15:54:05 +0800

    Remove unused property in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'thisdot-gh-user-search';
-}
+export class AppComponent { }
+
+
