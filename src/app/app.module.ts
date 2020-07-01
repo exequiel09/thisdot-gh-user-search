db897984ae6f1bd59d046820ac714c92a9b253c9
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-07-01 11:58:29 +0800

    Import HttpClientModule to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
     ClarityModule,
 
