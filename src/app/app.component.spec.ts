d96c0e2432297e9d30709322c7bea75a08a4645e
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-07-02 18:23:08 +0800

    Remove unnecessary tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { ClarityModule } from '@clr/angular';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ClarityModule,
       ],
       declarations: [
         AppComponent
@@ -20,18 +24,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'thisdot-gh-user-search'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('thisdot-gh-user-search');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('thisdot-gh-user-search app is running!');
-  });
 });
 
 
